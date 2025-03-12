9b34816609280849a09e2cc62718d78d7317eb96
Andre Moreno <80718143+More3428@users.noreply.github.com>
2025-03-11 20:52:48 -0700

    FileSystem
    Need to update path to have functionality.

---
+++ MIMs/src/main/java/com/example/MIMs/controller/FileUploadController.java
@@ -0,0 +1,43 @@
+package com.example.MIMs.controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.MIMs.service.StorageService;
+
+@RestController
+@RequestMapping("/image")
+public class FileUploadController {
+
+    @Autowired 
+    private StorageService service; 
+
+    @PostMapping("/fileSystem")
+    public ResponseEntity<?> uploadImageToFileSystem(@RequestParam("image")MultipartFile file) throws IOException {
+        String uploadImage = service.uploadImageToFileSystem(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/fileSystem/{fileName}")
+    public ResponseEntity<?> downloadImageFromFileSystem(@PathVariable String fileName) throws IOException {
+        byte [] imageData= service.downloadImageFromFileSystem(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("/image.png"))
+                .body(imageData);
+    }
+
+
+}
